faf2a714f013dcc4b2d643fd23d1f3381b0e8e04
Tanishka-Jagtap <131904030+Tanishka-Jagtap@users.noreply.github.com>
2024-01-26 18:03:34 +0530

    Update SignupForm.jsx

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,20 +1,50 @@
 import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from "../firebase-config";
 
 
 const SignupForm = () => {
     const [passVisible, setPassVisible] = useState(false);
     const [password, setPassword ] = useState('');
+    const [email, setEmail] = useState('');
     const [confpassword, setConfPassword ] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+          const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+          console.log(userCredential);
+          const user = userCredential.user;
+          localStorage.setItem('token', user.accessToken);
+          localStorage.setItem('user', JSON.stringify(user));
+          navigate("/");
+      } catch (error) {
+          console.error(error);
+      }
+  }
 
   return (
     <div className='min-h-screen flex items-center justify-center'>
-    <form className='bg-yellow-300 p-8 rounded shadow-md w-96'>
+    <form onSubmit={handleSubmit} className='bg-yellow-300 p-8 rounded shadow-md w-96'>
         <h2 className='text-2xl font-bold mb-4'>Create a new Account</h2>
-        <input type='text' className='mt-2 p-2 w-full border border-gray-300 rounded' placeholder='username' />
+        <input 
+        type='email' 
+        className='mt-2 p-2 w-full border border-gray-300 rounded' 
+        placeholder='username' 
+        required 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)}
+        />
         <input type={passVisible ?  'text' : 'password'}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        className='mt-2 p-2 w-full border border-gray-300 rounded' placeholder='password' />
+        className='mt-2 p-2 w-full border border-gray-300 rounded' 
+        placeholder='password' 
+        required
+        />
         <input type={passVisible ?  'text' : 'password'}
         value={confpassword}
         onChange={(e) => setConfPassword(e.target.value)}
